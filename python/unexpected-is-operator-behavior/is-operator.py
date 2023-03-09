8e3aaad286aecf509cfe11a2e67f6eb92bbffe08
Robin Rosiers <5263188+RosiersRobin@users.noreply.github.com>
2023-03-09 12:02:45 +0100

    Add is operator with unexpected behavior

---
+++ python/unexpected-is-operator-behavior/is-operator.py
@@ -0,0 +1,8 @@
+a = [1, 2, 3]
+b = a
+print(a is b) # Output: True
+
+a.append(4)
+print(a) # Output: [1, 2, 3, 4]
+print(b) # Output: [1, 2, 3, 4]
+print(a is b) # Output: True, even though the objects are no longer the same
