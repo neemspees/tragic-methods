e55160ee68969bb9d1c7f1daac9c3214bbfb0708
Aleksandr Frolov <thephilosoft@gmail.com>
2023-03-10 08:53:10 -0500

    PHP type juggling remediation mention

--- php/type-juggling/typeJuggler.php
+++ php/type-juggling/typeJuggler.php
@@ -9,3 +9,8 @@ $values = ["mommy", "daddy", "stepsister", "big brother"];
 var_dump(in_array(0, $values));
 
 // returns bool(true)
+
+$values = ["mommy", "daddy", "stepsister", "big brother"];
+var_dump(in_array(0, $values, true));
+
+// returns bool(false)
